d6c6f77421b98036a19f1c3f363c26dbfad57f83
Steve Guerrero <s_guerrero1@u.pacific.edu>
2017-02-16 08:56:24 -0800

    crossword output

--- lab04/main.c
+++ lab04/main.c
@@ -1,79 +1,21 @@
-/// This is a crossword puzzle program written in C
+// this is a test file
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "function.h"
-
-
-
-
-struct clue
-{
-	
-	char GREEN[24]="The color of most grass";
-	char GROWS[25]="This is what grass does";
-	char RARE[23]="A hard to find object";
-	char NASA[37]="A space program with little funding";
-	
-
-};
-
-void pound_array()
-
-
-void readfile() // this file will read in the crossword text file
-{ 
-
-strcasecmp();
-
-
-
-}
-
 
 int main()
 {
-	//char Blank[16][16];
-	
-	//char CW[6][6] = {GREEN};
-	
-	/*for (int i=0; i<=15; i++)
-	{
-		for (int j=0; j<=15; j++)
-		{
-		
-			printf("5X5 CROSSWORD PUZZLE \n \n Blank[%c][%c]", i,j);	
-		}
-		
-	}
-	*/
-	///if (
-	printf("Harro WorD \n");
-/*
-///example file in class
-char filename[256];
-FILE *fp = NULL;
-printf("Enter filename of game:";
-scanf("%255s",filename);
-fp=fopen(filename, "r");
-int rows , cols, clues;
-count = fscanf(fp,"%i %i %i\n", &rows, &cols, &clues); //read in 1st line count should =3,check
-count = fscanf(fp, "%i %i %i %255s %255[^\n]", &var1, &var2, &var3, var4, var5); // read in each clue line//test if count=5
+    FILE *ptr_file;
+    char buf[1000];
 
-
-*/
-
-
-
-
-
-
-
-
-
-	system ("PAUSE");
-	return 0;
-
+    ptr_file =fopen("crossword_puzzle.txt" ,"r");
+    if  (!ptr_file)
+        return 1;
+    while (fgets(buf, 1000, ptr_file)!=NULL)
+        printf("%s",buf);
+    fclose(ptr_file);
+    return 0;
 
 }
+
